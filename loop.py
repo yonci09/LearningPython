e22e1ce59ecab15b026e11632011f32ded5ce138
Yonca Aksit <126642768+yonci09@users.noreply.github.com>
2023-09-24 21:13:08 -0700

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,18 @@
+
+#While loop
+# print from 10 to 1
+
+num = 10
+ 
+while num != 0:
+    print(num)
+    num -= 1
+
+
+
+
+#for loop 
+sentence = "Python Learning :Practicing Loop"
+ 
+for letter in sentence:
+    print(letter)
