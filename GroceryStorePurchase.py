778240861f911ac4ab0a5f8ec85d48f91529fa96
Yonca Aksit <126642768+yonci09@users.noreply.github.com>
2023-09-24 20:12:42 -0700

    Add files via upload

---
+++ GroceryStorePurchase.py
@@ -0,0 +1,18 @@
+#declaring variables and assigning prices to them
+
+
+penne =  16.68
+sauce = 6.98
+garlic = 16.78
+italian_seasoning = 15.26
+bread = 3.0
+meatballs = 4.39
+
+
+#calculating subtotal of all items.
+sub_total = (penne + sauce + garlic + italian_seasoning + bread + meatballs)
+
+#printing the subtotal
+print(sub_total)
+
+print(round(sub_total,2))
