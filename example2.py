fdd9d8f5fd77cd8f8df0fe1248868e596fb969af
Yonca Aksit <126642768+yonci09@users.noreply.github.com>
2023-09-24 19:41:27 -0700

    example2.py

--- example2.py
+++ example2.py
@@ -1,3 +1,4 @@
+#declaring variables and assigning
 x = 3
 y = 4.03
 z = True
@@ -9,17 +10,19 @@ exponen = 4**4   #256
 floor_division = 16//5   # 3
 modulo = 7 %3  #1 the remainder is left 7 is divided by 3
 
-
+# exercise1 
 sum_num = 3 + 2
 dif_num = 5 - 2
 div_num = 12 / 2
 mul_num = 5 * 3
 power_num = 3**8
 floor_div = 10 // 3
-modulo_num = 17 % 15
+modulo_num = 17 % 12
 
 # print
+print(sum_num)
 
+#exercise2
 firstNum = 4
 print(firstNum)
 print( 5+2 /1)
