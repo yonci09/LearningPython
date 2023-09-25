5770ef3e9523795e6706e123347ecc8b293b21be
Yonca Aksit <126642768+yonci09@users.noreply.github.com>
2023-09-24 20:24:46 -0700

    Add files via upload

---
+++ Functions.py
@@ -0,0 +1,12 @@
+#creating a function
+
+
+def name_printer(first_name):
+    print(first_name)
+ 
+
+# requesting information from the user by asking question
+name = input("Please enter your name.")
+
+#recalling the function to print
+name_printer("Your Name is " + name)
---
+++ Input.py
@@ -0,0 +1,12 @@
+# What is your name?
+#What is your favorite car brand?
+#What is your favorite color? 
+#ask user these questions and store answers
+
+
+
+first_name = input("What is your name?")
+car_brand = input("What is your favorite car brand?")
+fav_color = input("What is your favorite color?")
+ 
+print("Your name is " + first_name + ", your fav color is " + fav_color + ", and your favorite car brand is " + car_brand + ".")
